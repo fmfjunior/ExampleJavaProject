b64007275999f89f0a859fbbddb4254008ba9a3e
Francisco Marques <fmfjunior@gmail.com>
2020-05-09 19:10:06 -0300

    Commmit inicial

---
+++ src/main/java/SonarCoverageTest/SonarCoverageTest/Calculation.java
@@ -0,0 +1,14 @@
+package SonarCoverageTest.SonarCoverageTest;
+/*
+* @author Umashankar
+* http://j2eedev.org
+* Beginer JUnit Tutorial
+* */
+public class Calculation {
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	public static int sub(int a, int b) {
+		return a - b;
+	}
+}
